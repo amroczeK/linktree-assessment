7bd3fd821a8e77092ecb7d8dae3f0ed81ff9fe1b
amroczeK <mroczek9990@gmail.com>
2022-04-22 23:09:16 +0800

    assests, config update, dep updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {},
+  },
+  images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   plugins: [
     require("@tailwindcss/typography"),
-    require("@tailwindcss/line-clamp")
+    require("@tailwindcss/line-clamp"),
   ],
 };
