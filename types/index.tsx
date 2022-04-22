3b563f9bce51bd789b6c5792d60224b50bf376e2
amroczeK <mroczek9990@gmail.com>
2022-04-22 12:02:35 +0800

    link types, user model

---
+++ types/index.tsx
@@ -0,0 +1,50 @@
+export interface ISocialLink {
+    socialsName: string;
+    url: string;
+}
+
+export interface ISocials extends Array<ISocialLink>{
+    [index: number]: ISocialLink;
+}
+
+export interface IPlatformDetails {
+    platformName: string;
+    url: string;
+}
+
+export interface IPlatforms extends Array<IPlatformDetails>{
+    [index: number]: IPlatformDetails;
+}
+
+export interface IMusic {
+  songName: string;
+  platforms: IPlatforms;
+}
+
+export interface IShowDetails {
+  date: Date;
+  location: string;
+  ticketsAvailable: boolean;
+}
+
+export interface IShows extends Array<IShowDetails>{
+    [index: number]: IShowDetails;
+}
+
+export interface ILinks {
+    socials: ISocials;
+    music: IMusic;
+    shows: IShows;
+}
+
+export interface IDesignPreferences {
+    backgroundColor: string;
+    primaryColor: string;
+    secondaryColor: string;
+}
+
+export interface User {
+  id: string;
+  designPreferences: IDesignPreferences;
+  links: ILinks;
+}
