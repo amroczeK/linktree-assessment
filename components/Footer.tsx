9d66ab8409f6f2df11bedada34910f001b6793e8
amroczeK <mroczek9990@gmail.com>
2022-04-23 16:31:24 +0800

    reusable footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from "react";
+import NextLink from "./NextLink";
+import Image from "next/image";
+
+const Footer: FC = () => {
+  return (
+    <div className="absolute inset-x-0 bottom-0 flex justify-center items-center">
+      <NextLink href="/">
+        <Image
+          src="/assets/logo.svg"
+          height={64}
+          width="100%"
+          alt="linktree-logo"
+        />
+      </NextLink>
+    </div>
+  );
+};
+
+export default Footer;
